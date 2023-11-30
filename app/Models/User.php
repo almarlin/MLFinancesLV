648f4855822767dd4bab15c8a5353c2c4a618f53
Alvaro Martinez <alvaro.martinez.lineros@gmail.com>
2023-11-30 10:48:18 +0100

    Seeder usuario Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'PC',
         'ADDRESS',
         'PHONENUMBER',
-        'HASH',
+        'passsword',
         'BAN',
         'ADMIN',
         'PROFILEPHOTO'
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $adminUser = new User();
+        $adminUser->NIF=000000000;
+        $adminUser->NAME = 'admin';
+        $adminUser->SURNAME='admin';
+        $adminUser->email = 'admin@admin.com';
+        $adminUser->password = Hash::make('admin');
+        $adminUser->ADMIN = true;
+
+        $adminUser->save();
     }
 }
