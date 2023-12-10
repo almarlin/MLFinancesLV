301db36fdd31a7ee97e376592f3a662b4f7a2374
Álvaro <alvaro.martinez.lineros@gmail.com>
2023-12-10 11:39:56 +0100

    Portal de administración

--- resources/views/mainAdmin.blade.php
+++ resources/views/mainAdmin.blade.php
@@ -1,8 +1,95 @@
 @extends('layouts.template')
-
+@section('navbar')
+    @extends('layouts.navbarhome')
+@endsection
 
 @section('title', 'Panel de administración')
 
 @section('content')
+
+    <div class="container mt-5">
+        <h1 class="text-center display-5">Portal de administración</h1>
+
+        <h2 class="fw-light fs-3 mb-4">Últimos movimientos</h2>
+        <div class="row justify-content-center align-items-center g-2">
+            @php
+                use App\Models\Movement;
+                $bankMovements = Movement::all();
+                $count = 0;
+            @endphp
+            @foreach ($bankMovements as $bankMovement)
+                <div class="container rounded border border-2 border-danger movimiento mb-2">
+                    <p class="text-center">De {{ $bankMovement->fromIBAN }}</p>
+                    <p class="text-center">A {{ $bankMovement->toIBAN }}</p>
+                    <p class="text-center">Concepto {{ $bankMovement->CONCEPT }}</p>
+                    <p class="text-center">Cantidad {{ $bankMovement->QUANTITY }}</p>
+                </div>
+                @php
+                    $count++;
+                    if ($count == 2) {
+                        break;
+                    }
+                @endphp
+            @endforeach
+            <a href="#">Ver todos los movimientos</a>
+        </div>
+
+        <h2 class="fw-light fs-3 mt-4 mb-4">Cuentas</h2>
+        <div class="row justify-content-center align-items-center g-2">
+            @php
+                use App\Models\Account;
+                use App\Models\User;
+                $bankAccount = Account::all();
+                $count = 0;
+            @endphp
+            @foreach ($bankAccount as $bankAccount)
+                @php
+                    $accountUser = User::where('id', $bankAccount->user_id);
+                @endphp
+                <div class="container rounded border border-2 border-danger movimiento mb-2">
+                    <p class="text-center">IBAN {{ $bankAccount->IBAN }}</p>
+                    <p class="text-center">Balance {{ $bankAccount->BALANCE }}</p>
+                    <p class="text-center">Fecha de creación {{ $bankAccount->created_at }}</p>
+                    <p class="text-center">Titular {{ $accountUser->first()->NIF }}</p>
+                </div>
+                @php
+                    $count++;
+                    if ($count == 2) {
+                        break;
+                    }
+                @endphp
+            @endforeach
+            <a href="#">Ver todos las cuentas</a>
+        </div>
+
+        <h2 class="fw-light fs-3 mt-4 mb-4">Usuarios</h2>
+        <div class="row justify-content-center align-items-center g-2">
+            @php
+
+                $bankUsers = User::all();
+                $count = 0;
+            @endphp
+            @foreach ($bankUsers as $bankUser)
+                @if($bankUser->NAME!='admin')
+                    <div class="container rounded border border-2 border-danger movimiento mb-2">
+                        <p class="text-center">NIF {{ $bankUser->NIF }}</p>
+                        <p class="text-center">Nombre {{ $bankUser->NAME }}</p>
+                        <p class="text-center">Fecha de creación {{ $bankUser->created_at }}</p>
+                        <p class="text-center">Número de teléfono {{ $bankUser->PHONENUMBER }}</p>
+                    </div>
+                    @php
+                        $count++;
+                        if ($count == 2) {
+                            break;
+                        }
+                    @endphp
+                @endif
+
+                
+            @endforeach
+            <a href="#">Ver todos los usuarios</a>
+        </div>
+
+    </div>
 
 @endsection
