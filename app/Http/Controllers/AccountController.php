a1683245636ccde743cfcbde2c155ed573b4c3dc
Álvaro <alvaro.martinez.lineros@gmail.com>
2023-12-11 16:23:19 +0100

    Numero de cuenta. Inicio de correccion

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -9,23 +9,26 @@ class AccountController extends Controller
 {
     public function generateIban($userName)
     {
-
-        
-        $binary="";
-
-        for($i=0;$i<4;$i++){
-
-            $letter=$userName[$i];
-            $position=ord(strtoupper($letter)) - ord('A') + 1;
-            $binary.=$binary.decbin($position);
+        $binary = "";
+        $userName=str_replace(" ","",$userName);
+    
+        // Asegurarse de que el nombre de usuario tiene al menos 4 caracteres
+        if (strlen($userName) < 4) {
+            // Manejar este caso según sea necesario
+            return null;
         }
-        while(Account::where('IBAN',$binary)->exists()){
-            $binary.="1";
-           
+    
+        for ($i = 0; $i < 4; $i++) {
+            $letter = $userName[$i];
+            $position = ord(strtoupper($letter)) - ord('A') + 1;
+            $binary .= decbin($position);
         }
-        
+    
+        while (Account::where('IBAN', $binary)->exists()) {
+            $binary .= "1";
+        }
+    
         return $binary;
-
     }
 
     public function adminAccounts(){
