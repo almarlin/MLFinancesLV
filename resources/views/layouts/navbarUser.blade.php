645b0efac712823adaa5504402234fffffd96573
Álvaro <alvaro.martinez.lineros@gmail.com>
2023-12-13 23:50:24 +0100

    Navbar de home

--- resources/views/layouts/navbarUser.blade.php
+++ resources/views/layouts/navbarUser.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-danger bg-danger">
     <div class="container">
-        <a class="navbar-brand" href="#">ML Finances</a>
+        <a class="navbar-brand" href="{{route('index')}}">ML Finances</a>
         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
             aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
